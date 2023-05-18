ae8b7867aa7c573e28aa127538355df205710566
khadjaa <islomkhadja2001@icloud.com>
2023-05-18 16:52:20 +0300

    added useEffects and deleted StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
     const [value, setValue] = useState<number>(0)
 
-    const incHandler = () => {
-        setValue(value + 1)
-    }
-    const setToLocalStorage = () => {
-        localStorage.setItem('counterValue', JSON.stringify(value))
-    }
-    const getFromLocalStorage = () => {
+    useEffect(() => {
+        debugger
         let valueAsString = localStorage.getItem('counterValue')
         if (valueAsString) {
             let newValue = JSON.parse(valueAsString)
             setValue(newValue)
         }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value])
+
+    const incHandler = () => {
+        setValue(value + 1)
     }
 
     return (
         <div className="App">
             <h1>{value}</h1>
             <button onClick={incHandler}>inc</button>
-            <button onClick={setToLocalStorage}>setToLocalStorage</button>
-            <button onClick={getFromLocalStorage}>getFromLocalStorage</button>
         </div>
     );
 }
