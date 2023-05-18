fa7fb2bf4b65b5f1cca0ee8ff1838b052dae1db1
khadjaa <islomkhadja2001@icloud.com>
2023-05-18 16:11:24 +0300

    added function to set and get from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState<number>(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+    const setToLocalStorage = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }
+    const getFromLocalStorage = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorage}>setToLocalStorage</button>
+            <button onClick={getFromLocalStorage}>getFromLocalStorage</button>
+        </div>
+    );
 }
 
 export default App;
