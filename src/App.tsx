17a43a7126576854a3fd0bf9662c04acde9b738b
khadjaa <islomkhadja2001@icloud.com>
2023-05-18 18:42:37 +0300

    added inputs

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,48 @@ import './App.css';
 
 function App() {
 
+    const [minValue, setMinValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(0)
     const [value, setValue] = useState<number>(0)
 
     useEffect(() => {
-        debugger
-        let valueAsString = localStorage.getItem('counterValue')
-        if (valueAsString) {
-            let newValue = JSON.parse(valueAsString)
-            setValue(newValue)
-        }
+        resetHandler()
     }, [])
 
     useEffect(() => {
-        localStorage.setItem('counterValue', JSON.stringify(value))
-    }, [value])
+        setHandler()
+    }, [minValue])
 
     const incHandler = () => {
         setValue(value + 1)
     }
+    const resetHandler = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setMinValue(newValue)
+        }
+    }
+    const setHandler = () => {
+        localStorage.setItem('minValue', JSON.stringify(minValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+    }
 
     return (
         <div className="App">
-            <h1>{value}</h1>
-            <button onClick={incHandler}>inc</button>
+            <div className='view'>
+                <h1>{minValue}</h1>
+                <button onClick={incHandler}>inc</button>
+                <button onClick={() => {}}>reset</button>
+            </div>
+            <div className='set'>
+                <input type="number"
+                       onClick={incHandler}
+                       value={minValue}/> min value
+                <input type="number"
+                       value={maxValue}/> max value
+                <button onClick={setHandler}>set</button>
+            </div>
         </div>
     );
 }
